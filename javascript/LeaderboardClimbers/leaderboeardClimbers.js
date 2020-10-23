660ac534073c2cde2e1adb3a3328902add0e0852
Jessica Sanchez <37550925+JessSanchezC@users.noreply.github.com>
2020-10-23 13:58:56 +0200

    Update leaderboeardClimbers.js
    Final solution submitted to codewars.com

--- javascript/LeaderboardClimbers/leaderboeardClimbers.js
+++ javascript/LeaderboardClimbers/leaderboeardClimbers.js
@@ -1,20 +1,20 @@
 /*
 source: https://www.codewars.com/kata/5f6d120d40b1c900327b7e22
+solved by: JASC and LZ 
+date: 23/10/2020
 */
 
 function leaderboardSort(leaderboard, changes) {
   //loop the changes array
-  for (var i = 0; i < changes.length; i++){
-    
-  //each index, split array index to have [name,movement]
-    var a_movement = changes[i].split(" ");
-    
-  //change leaderboard  
-    var index_Name = leaderboard.indexOf(a_movement[0));
-    var movement = parseInt(a_movement[1])
-  //change array
-    
-  //TODO
-  }
-  return //TODO
+  changes.forEach((elem) =>{
+    //each index, split to array [name,movement]
+    var a_movement = elem.split(" ");    
+    // get change parameters 
+      var index_Name = leaderboard.indexOf(a_movement[0]);
+      var new_index = index_Name + parseInt(a_movement[1])*-1
+    //change leaderboard
+      leaderboard.splice(new_index,0,...leaderboard.splice(index_Name,1))      
+  })   
+  
+  return leaderboard
 }
